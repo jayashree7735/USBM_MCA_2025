6e2dc0413b4a06bc4ea35464feda1b928ca15d6f
Jayashree Rout <jrout911@gmail.com>
2024-11-08 10:15:07 +0530

    Auth Updated

--- Javascript/projects/authentication/index.js
+++ Javascript/projects/authentication/index.js
@@ -1,23 +1,33 @@
-//To manipulate html elements
 
-// const username = document.getElementById("username").value;
-// const useremail = document.getElementById("email").value;
-// const userpassword = document.getElementById("password").value;
-//Don't use these above as it is outside the function so it can't be function inside button
-
-
-//To manipulate html elements
 const users = JSON.parse(localStorage.getItem("users")) || [];
-const signupbtn = document.getElementById("signup");
 
-//Add events
-signupbtn.addEventListener("click",auth);
+//To manipulate html elements
 
-function auth(){
-const username = document.getElementById("username").value;
-const useremail = document.getElementById("email").value;
-const userpassword = document.getElementById("password").value;
-    alert("It is working");
+const signupBtn = document.getElementById("signupBtn");
+const loginBtn = document.getElementById("loginBtn");
+const logoutbtn = document.getElementById("logoutbtn")
+
+
+//addevent
+signupBtn.addEventListener("click",signup);
+loginBtn.addEventListener("click",login);
+logoutbtn.addEventListener("click",logout)
+
+
+
+//Display dashboard if the user is logged in
+function showView(view){
+ document.getElementById("signup").style.display= view ==="signup" ? "block" :"none";
+ document.getElementById("login").style.display= view ==="login" ? "block" :"none";
+ document.getElementById("dashboard").style.display= view ==="dashboard" ? "block" :"none";
+ }
+
+//Signup function
+
+function signup(){
+    const username = document.getElementById("username").value;
+    const useremail = document.getElementById("email").value;
+    const userpassword = document.getElementById("password").value;
     console.log(username ,useremail ,userpassword );
 
     const newUser = {
@@ -25,10 +35,53 @@ const userpassword = document.getElementById("password").value;
         email: useremail,
         password:userpassword,
     };
+
+    //check if user alreaty exist
+    const userExits = users.find((user) => user.name === username);
+    if(userExits){
+        alert("Username already exits.Please choose anathor name.")
+        return;
+    }
+
+
     users.push(newUser);
     localStorage.setItem("users",JSON.stringify(users));
-    console.log(newUser);
+    alert("sign up successfull!!")
+    showView("login");
 
 }
 
-// console.log(username ,useremail ,userpassword );
+//login function
+function login(){
+const useremail = document.getElementById("email").value;
+const userpassword = document.getElementById("password").value;
+
+const storeUsers = JSON.parse(localStorage.getItem("users")) || [];
+const users = storeUsers.find((user)=> user.email === useremail && user.password === userpassword);
+ if(users){
+    const loogedUser = localStorage.setItem("looggedUser", JSON.stringify(users));
+    alert("Login succesfull");
+    showDashboard();
+ }
+    else {
+       alert("Invalid credentials");
+    }
+}
+
+//dashboard function
+function showDashboard(){
+    const user = JSON.parse(localStorage.getItem("looggedUser"));
+    document.getElementById("usernameDisplay").innerText = `Hey ${user.name}, Welcome !!`;
+    showView("dashboard")
+}
+
+
+//logout
+// logout.addEventListener("click",logout);
+
+function logout(){
+    localStorage.removeItem("loggedUser");
+    alert("You've successfully logged out!!");
+    showView("signup");
+
+}
