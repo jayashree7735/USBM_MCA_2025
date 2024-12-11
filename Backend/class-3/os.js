a7588650a68e6e146cb48fb03753cfbee79270c2
Jayashree Rout <jrout911@gmail.com>
2024-12-11 12:15:02 +0530

    os_module

---
+++ Backend/class-3/os.js
@@ -0,0 +1,15 @@
+//import modules
+
+const os = require("os");
+
+
+
+console.log(os);
+console.log(os.homedir()); //C:\Users\91859
+console.log(os.hostname());//LAPTOP-DRNGV5EN
+console.log(os.platform());//win32
+console.log(os.release());//10.0.22631
+console.log(os.userInfo());
+console.log(os.totalmem());//6387105792
+console.log(os.type());//Windows_NT
+
